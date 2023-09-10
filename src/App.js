daec59ff62768e24e28e49b3b29b08a8deb758b9
VastSea0 <vastseaoffical0@outlook.com>
2023-09-10 17:48:37 +0300

    my-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <img src={logo}/>
+    
+    </div>
+  );
+}
+
+export default App;
